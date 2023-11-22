9bc9d5b6e4e40a51658f02ca8f679281fa12f3c5
marimain-art <marian.mainarte@gmail.com>
2023-11-21 23:45:59 -0300

    Exercício Pokemon

--- Exercícios_pokemon/Capturar_pokemons/js/script.js
+++ Exercícios_pokemon/Capturar_pokemons/js/script.js
@@ -1,33 +1,27 @@
 const POKURL = 'https://pokeapi.co/api/v2/pokemon/'
 
-function getPokemonList(){
-    let pokemonsList = []
-    fetch(POKURL + '?limit=151').then( response => {
+let pokedex = []
+    fetch(POKURL + '?limit=251').then( response => {
         if(response.status == 200){
-            pokemonsList = response.json().then( json => {
-                console.log(json.results.map( pokemon => { console.log(pokemon.name)})) 
-                return json})
-            return pokemonsList
-        }
-    })
-}
-
-function showPokemons(){
-    let pokemonsList = []
-    fetch(POKURL + '?limit=151').then( response => {
-        if(response.status == 200){
-            pokemonsList = response.json().then( json => {
-                console.log(json.results.map( pokemon => { 
-                    let liPokemon = document.createElement("li")
-                    let liPokemonImg = document.createElement("img")
-                    fetch(pokemon.url).then( response => { pokemonImg = response.json().then( (pokemon) => {(liPokemonImg.src = pokemon.sprites.front_default)})})
-                    liPokemon.innerHTML = pokemon.abilities
-                    liPokemon.appendChild(liPokemonImg)
-                    document.getElementById("pokemonList").appendChild(liPokemon)
-                 })) 
-                return json
+            pokedex = response.json().then( api => {
+                console.log(api.results.map( pokLi => {
+                        let pokemonLi = document.createElement("li")
+                        let pokemonIcon = document.createElement("img")
+                        let pokemonName = document.createElement("h3")
+                        fetch(pokLi.url).then( pokInfo => { pokeIcon = pokInfo.json().then( (pokImg) => {(pokemonIcon.src = pokImg['sprites']['versions']['generation-vii']['icons']['front_default'])}).then( (pokId) => {(pokmnId.innerHTML = pokId.id)})})
+                        pokemonName = pokLi.name
+                        pokemonLi.innerHTML = 
+                        `
+                            <div id="pokLi">
+                                <a href="#pokModal">${pokemonName}</a>
+                                <img src="${pokemonIcon.src}" class="pokIcon">
+                            </div>
+                        `
+                        pokemonLi.appendChild(pokemonIcon)
+                        document.getElementById("pokedexList").appendChild(pokemonLi)
+                }))
+                return api
             })
-            return pokemonsList
+            return pokedex
         }
     })
-}
